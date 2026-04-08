1f99ddb5d7df587cd544b0e22c0998ea3fddb9ef
uoahoosk-commits <uoahoosk@gmail.com>
2026-04-08 18:36:17 +0900

    Update 수행평가 연습1_20601_강수연.c

--- 수행평가 연습1_20601_강수연.c
+++ 수행평가 연습1_20601_강수연.c
@@ -4,8 +4,8 @@ int main() {
     int dan = 5; 
     int i;
 
-    for (i = 0; i < 9; i++) { // 반복문 시작할 떄 1이어야 함
-        printf("%d * %d = %d\n", dan, i, dan * i) //세미콜론이 빠짐(컴파일러오류생김)
+    for (i = 1; i < 10; i++) { // 반복문 시작할 떄 1이어야 함
+        printf("%d * %d = %d\n", dan, i, dan * i); //세미콜론이 빠짐(컴파일러오류생김)
     }
 
     return 0;
